dd69d18f5e18e9f0f52a15ef90571bfbaa437c09
Blake Eldridge <blakeldridge@gmail.com>
2026-08-16 20:04:10 +0100

    Home page todos complete, new links, testimonials and links added

--- absent-azimuth/src/consts.ts
+++ absent-azimuth/src/consts.ts
@@ -21,6 +21,54 @@ export const REVIEWS = [
         rating: 5,
         comment: "Incredibly helpful—explained complex topics clearly, boosted my confidence, and made learning enjoyable. I improved a lot in the span of 2 weeks before my exams!",
         date: "May 2025"
+    },
+    {
+        name: "Swetna G.",
+        rating: 5,
+        comment: "Blake is very engaging and Know how to raise a My Sons confidence which was very low . Blake is great tutor and my son enjoys his teaching and his happy how he is progressing.",
+        date: "Jul 2026"
+    },
+    {
+        name: "Kwokkeung C.",
+        rating: 5,
+        comment: "Blake has been an outstanding tutor for my daughter in GCSE Computer Science. He explains topics clearly, makes lessons engaging, and helped her build real confidence in the subject. With his support, she was able to achieve a very strong grade in her exam. We are very pleased with his teaching and highly recommend him to other students.",
+        date: "Sep 2025"
+    },
+    {
+        name: "Sasha W.",
+        rating: 5,
+        comment: "Great first lesson. Lawrence really enjoyed it. Thank you.",
+        date: "Aug 2025"
+    },
+    {
+        name: "Caroline F.",
+        rating: 5,
+        comment: "Blake has been a brilliant inspiration to my son and his enthusiasm for computing excellent. It helped my son achieve highly in coding and also improved his theory knowledge. My son enjoyed time with Blake so much he is now doing A level!",
+        date: "Jul 2025"
+    },
+    {
+        name: "Maxance L.",
+        rating: 5,
+        comment: "Super helpful, He is very kind and friendly and has a great work ethic. He has been extremely helpful and supportive throughout my exams whiles being flexible when it came to booking times and availability saving me when it came to very short notice last minute revision sessions.",
+        date: "Jun 2025"
+    },
+    {
+        name: "Maryam Z.",
+        rating: 5,
+        comment: "I started A Level Computer Science with no prior knowledge, but thanks to Blake, I built a solid foundation and gained confidence in both theory and programming. His clear explanations, patience, and structured approach made even complex topics easy to understand. I'm incredibly thankful for the guidance he provided throughout my studies.",
+        date: "May 2025"
+    },
+    {
+        name: "Marija R.",
+        rating: 5,
+        comment: "We are very pleased with first lesson that my son had with Blake. He was very professional and thoughtful. My son think the lesson was very helpful and we'l definitely have more. Thank you Blake",
+        date: "Feb 2025"
+    },
+    {
+        name: "Ka Lai Debbie L.",
+        rating: 5,
+        comment: "Thank you Blake E. he taking the time to prepare content and worksheets to help my daughter learn. So Good!",
+        date: "Sep 2024"
     }
 ];
 export const EXAM_BOARDS = [
